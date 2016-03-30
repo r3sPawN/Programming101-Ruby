590c5df0cf5faf4e74ced5945ff64d8530d7323d
r3sPawN <markoff.g@gmail.com>
2016-03-30 10:44:15 +0300

    add fmi-tribute

--- week03/1-fmi-tribute/patch_array.rb
+++ week03/1-fmi-tribute/patch_array.rb
@@ -1,3 +1,17 @@
 class Array
-  # Your code goes here.
+  def to_hash
+    Hash[*flatten]
+  end
+
+  def index_by(&block)
+    inject({}) { |b, elements| b.merge!({ block.call(elements) => elements  })  }
+  end
+
+  def subarray_count(sub)
+
+  end
+
+  def occurence_count
+    zip(map{|items| count(items)}).to_hash
+  end
 end
